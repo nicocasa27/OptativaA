feb542707058fb0b50be4961b76bd15dff716af2
nicocasa27 <nicocasa27@gmail.com>
2023-02-07 08:43:33 -0600

    CLASE 1

---
+++ ProgramacionEstructurada/maina.java
@@ -0,0 +1,16 @@
+public class maina {
+
+public static void main(String[] args) {
+
+   int edad =18;
+   String nombre = "Nicolas";
+   System.out.println("Hola soy  "+nombre+" y tengo "+edad+" anios");
+
+
+   int anioss =2023-2004;
+   String nombredenacimiento = "Nicolas Casnueva";
+   System.out.println("Hola soy  "+nombredenacimiento+" y tengo "+anioss+" anios");
+
+    }
+}
+
